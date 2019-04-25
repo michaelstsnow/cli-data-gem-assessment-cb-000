11d2f32cfc81ffe7ef50f703c98127df4c6355ae
Michael Snow <msnow@gitlab.com>
2019-04-25 10:25:01 -0700

    adjusting process so that I have to provide all options and how they select since progblems with scraping react text

--- lib/CommandLineInterface.rb
+++ lib/CommandLineInterface.rb
@@ -11,7 +11,7 @@ class CommandLineInterface
 	#This Is the overall flow of the code and will be ogiginally mapped out in comments
 
 	#I trigger the program
-		# The program srapes the website for the available options and stores them 
+		# The program srapes the website for the available options and stores them  -> turns out that this is not as easy as I thought since it is a react portion of the website and instead will have to use constants
 	# Program asks me what options I want to scrape the website for
 		# The program displays each option 1 by one and asking if I want to filter by it Y/N 
 		# I provide my selection for various options
@@ -19,9 +19,20 @@ class CommandLineInterface
 	# Scraper returns the companies that fit my options or a message saying if there are no companies try adjusting my criteria 
 
     YComboBase="https://www.ycombinator.com/companies/"
+    All_Batches=["All Batches","w2019","s2018","w2018","s2017","w2017","s2016","w2016","s2015","w2015","s2014","w2014","s2013","w2013","s2012","w2012","s2011","w2011","s2010","w2010","s2009","w2009","s2008","w2008","s2007","w2007","s2006","w2006","s2005"]
+    All_Categories=["All categories","Artificial Intelligence","Augmented Reality","B2B","Biomedical","Biotech","Community","Consumer","Developer Tools","Diversity","Drones","Education","Energy","Enterprise","Entertainment","Fintech","Government","Hardware","Healthcare","Marketplace","Media","Other","Robotics","Science","Security","Transportation","Virtual Reality"]
+    All_Female_Team=false
+    Female_Founder=false
+
 
 	def run
-		get_options(YComboBase)
+		#greet the user with some helpful info
+		#let them know what the program does
+		#ask which options they would like to move forward with?
+			#Build final URL to scrape the site
+			#scape the website for companies based on their input and store
+		#return the list of companies or a message of there are no companies in their search criteria 
+
 		set_final_url
 		get_companies
 		display_companies
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,6 @@ class Scraper
 
 	def self.scrape_for_options(url)
 		doc = Nokogiri::HTML.parse(open(url))
-		all_options_house = doc.css('div.controls')
-		options = all_options_house.css('select option')
-		### Need to figure out the proper selectors on this website
-
 	end
 
 end
