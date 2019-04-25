bf0939344adf52229fb470013a6d3d5c8556d5a0
Michael Snow <msnow@gitlab.com>
2019-04-25 09:47:58 -0700

    pushing scraperrb to github

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ class Scraper
 		doc = Nokogiri::HTML.parse(open(url))
 		all_options_house = doc.css('div.controls')
 		options = all_options_house.css('select option')
+		### Need to figure out the proper selectors on this website
 
 	end
 
